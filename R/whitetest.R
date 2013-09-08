c322e8982f5a46c5da227089470d85fd40da3894
Bin Wang <bwang@southalabama.edu>
2013-09-08 00:00:00 +0000

    version 1.1-9-8

--- R/whitetest.R
+++ R/whitetest.R
@@ -23,19 +23,27 @@
 ## 189–202. doi:10.1257/jep.20.4.189.
 
 
+
 white.test <- function(lmobj)
 {
   stopifnot(class(lmobj)=='lm')
-  mydata = lmobj$model
-  mydata[,1] = lmobj$residual^2
-  fml = lmobj$call$formula
-  formula1 = paste(fml[2],fml[1],fml[3])
-  pvs = attr(lmobj$terms,"term.labels")
-  k = length(pvs)
+  mydata <- lmobj$model
+  mydata[,1] <- lmobj$residual^2
+  fml <- lmobj$call$formula
+  formula1 <- paste(fml[2],fml[1],fml[3])
+  pvs <- attr(lmobj$terms,"term.labels")
+  k <- length(pvs);
+  n <- length(lmobj$fit)
+  
   for(i in 1:k){
-    tmp <- NULL
-    for(j in 1:nchar(pvs[i])){
-      tmp1 <- substr(pvs[i],j,j)
+    tmp <- NULL;
+    if(substr(pvs[i],1,2)=="I("){
+      tmp2 <- substr(pvs[i],3, nchar(pvs[i])-1);
+    }else{
+      tmp2 <- pvs[i];
+    }
+    for(j in 1:nchar(tmp2)){
+      tmp1 <- substr(tmp2,j,j)
       if(tmp1 == ":")
         tmp <- paste(tmp, "*", sep='')
       else
@@ -48,24 +56,32 @@ white.test <- function(lmobj)
     if(i>1)
       formula2 <- paste(formula2, "+", sep='')
     formula2 <- paste(formula2, "I(", pvs[i],")",sep='')
-
+    
     for(j in i:k)
-      formula2 = paste(formula2,"+I(",pvs[i],"*",pvs[j],")", sep='')
+      formula2 <- paste(formula2,"+I(",pvs[i],"*",pvs[j],")", sep='')
   }
-  ##  print(formula1)
-  out = lm(as.formula(formula2),data=mydata)
-  n = length(lmobj$fit)
-  LM = summary(out)$r.squared * n  
-  names(LM) <- "White"
-  df <- out$rank - 1
-  names(df) <- "df";
-  RVAL <- list(statistic = LM,
-      parameter = df,
-      method = "White test for constant variance",
-      p.value= pchisq(LM,df,lower.tail=FALSE),
-      data.name=NULL)
-
-  class(RVAL) <- "htest"
+  
+  out <- lm(as.formula(formula2),data=mydata)
+  ##  vna <- is.na(out$coef)
+  ##  if(any(vna)){
+  ##    warning("\nDuplicated regressor(s) removed.\n ",
+  ##            paste(names(out$coef[vna]),";"));
+  ##  }
+  if(summary(out)$r.squared == 1.0){
+    RVAL <- NULL;
+    warning("Test failed.  Possible reasons:\n\t (1) collinearity, or (2) sample size is not big enough for the White's test.");
+  }else{
+    LM = summary(out)$r.squared * n  
+    names(LM) <- "White"
+    df <- out$rank - 1
+    names(df) <- "df";
+    RVAL <- list(statistic = LM,
+                 parameter = df,
+                 method = "White test for constant variance",
+                 p.value= pchisq(LM,df,lower.tail=FALSE),
+                 data.name=NULL)
+    class(RVAL) <- "htest"
+  }
   return(RVAL)
 }
 
